abc23d153050f89bce50aabc0360d03c4a8a3c89
kc71486 <linda20030102@gmail.com>
2023-10-18 15:17:18 +0800

    change it into -o0

--- hw2/main.c
+++ hw2/main.c
@@ -3,7 +3,7 @@
 
 extern uint64_t get_cycles();
 
-extern int HammingDistance(uint64_t, uint64_t);
+extern int HammingDistance_c(uint64_t, uint64_t);
 
 uint64_t test1_x0 = 0x0013000000100000;
 uint64_t test1_x1 = 0x00000000000FFFFF;
@@ -14,13 +14,13 @@ uint64_t test3_x1 = 0x000000028370228F;
 
 int main(){
     int32_t start = get_cycles();
-    int32_t d1 = HammingDistance(test1_x0, test1_x1);
-    int32_t d2 = HammingDistance(test2_x0, test2_x1);
-    int32_t d3 = HammingDistance(test3_x0, test3_x1);
+    int32_t d1 = HammingDistance_c(test1_x0, test1_x1);
+    int32_t d2 = HammingDistance_c(test2_x0, test2_x1);
+    int32_t d3 = HammingDistance_c(test3_x0, test3_x1);
     int32_t end = get_cycles();
     printf("Elapse cycle:%ld\n", end - start);
-    printf("Hamming Distance:%ld\n", d1);
-    printf("Hamming Distance:%ld\n", d2);
-    printf("Hamming Distance:%ld\n", d3);
+    printf("Hamming Distance:%ld\n", d1); // 24
+    printf("Hamming Distance:%ld\n", d2); // 62
+    printf("Hamming Distance:%ld\n", d3); // 0
     return 0;
 }
