c4f243ad7e23955229915f7f8b2a045eb85d0866
kc71486 <linda20030102@gmail.com>
2023-10-22 22:30:57 +0800

    add comment to print.h

--- hw2/print.h
+++ hw2/print.h
@@ -1,10 +1,26 @@
 # ifndef PRINT_H
 # define PRINT_H
 
-void printstr(const char *, uint32_t);
+/*
+ * Print string into stdout.
+ * params:
+ *   str: printed string
+ *   strsize: size of string (includes ending '\0')
+ */
+void printstr(const char *str, uint32_t strsize);
 
-void printchar(char);
+/*
+ * Print a single character into stdout.
+ * params:
+ *   input: input character
+ */
+void printchar(char input);
 
-void printint(int32_t);
+/*
+ * Print an integer into into stdout.
+ * params:
+ *   input: input integer
+ */
+void printint(int32_t input);
 
 #endif
