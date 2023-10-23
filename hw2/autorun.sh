4a14cb55b70c4468aedd8a51d20ab6f309016741
kc71486 <linda20030102@gmail.com>
2023-10-23 14:58:20 +0800

    modify shell

--- hw2/autorun.sh
+++ hw2/autorun.sh
@@ -1,5 +1,10 @@
-riscv-none-elf-readelf -h hammingdistance.elf
-echo ""
+#!/bin/bash
+make
+if [[ $? -ne 0 ]]
+then
+  exit
+fi
+echo "program size:"
 riscv-none-elf-size hammingdistance.elf
-echo ""
+echo "execution result:"
 rv32emu hammingdistance.elf
