5f3ca72a7ce9e9d45c55246a06b713f1b7347760
kc71486 <linda20030102@gmail.com>
2023-10-28 01:35:23 +0800

    add moddiv

--- hw2/convert.c
+++ hw2/convert.c
@@ -1,6 +1,9 @@
 #include <stdint.h>
 #include "convert.h"
 
+extern uint32_t udiv32(uint32_t, uint32_t);
+extern uint32_t umod32(uint32_t, uint32_t);
+
 uint32_t itos(int32_t input, char *str, uint32_t strsize) {
     //otherwise it will not put anything
     if(input == 0) {
@@ -33,20 +36,22 @@ uint32_t itos(int32_t input, char *str, uint32_t strsize) {
     char buf[12] = {0};
     uint32_t bufidx = 0;
     uint32_t stridx = 0;
+    uint32_t uinput;
     if(input < 0) {
-        input = 0 - input;
-        while(input > 0) {
-            buf[bufidx] = (char) ((input % 10) + 48);
-            input /= 10;
+        uinput = 0 - input;
+        while(uinput > 0) {
+            buf[bufidx] = (char) (umod32(uinput, 10) + 48);
+            uinput /= udiv32(uinput, 10);
             bufidx += 1;
         }
         buf[bufidx] = '-';
         bufidx += 1;
     }
     else {
-        while(input > 0) {
-            buf[bufidx] = (char) ((input % 10) + 48);
-            input /= 10;
+        uinput = input;
+        while(uinput > 0) {
+            buf[bufidx] = (char) (umod32(uinput, 10) + 48);
+            uinput /= udiv32(uinput, 10);
             bufidx += 1;
         }
     }
---
+++ hw2/moddiv.s
@@ -0,0 +1,37 @@
+.global udiv32
+.global umod32
+
+.text
+.align 2
+# copy from gcc
+udiv32:
+    mv    a2, a1
+    mv    a1, a0
+    li    a0, -1
+    beqz  a2, 5f
+    li    a3, 1
+    bgeu  a2, a1, 2f
+1:
+    blez  a2, 2f
+    slli  a2, a2, 1
+    slli  a3, a3, 1
+    bgtu  a1, a2, 1b
+2:
+    li    a0, 0
+3:
+    bltu  a1, a2, 4f
+    sub   a1, a1, a2
+    or    a0, a0, a3
+4:
+    srli  a3, a3, 1
+    srli  a2, a2, 1
+    bnez  a3, 3b
+5:
+    ret
+
+# copy from gcc
+umod32:
+    mv  t0, ra
+    jal   udiv32
+    mv  a0, a1
+    jr    t0
