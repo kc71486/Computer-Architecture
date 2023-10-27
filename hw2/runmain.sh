b1c10b7dd45c1327d2f2ee20714f9457be6c1ef5
kc71486 <linda20030102@gmail.com>
2023-10-27 23:48:36 +0800

    modify

--- hw2/runmain.sh
+++ hw2/runmain.sh
@@ -4,7 +4,7 @@ function runprogram() {
   if [ $1 -ne 0 ]
   then
     echo "make error, exit"
-    exit
+    return
   fi
   echo "output into out${2} ..."
   echo "program size:" > "out${2}"
@@ -13,7 +13,6 @@ function runprogram() {
   rv32emu hammingdistance.elf >> "out${2}"
   echo "dump into dump${2} ..."
   riscv-none-elf-objdump -d hammingdistance.elf > "dump${2}"
-  make clean
 }
 
 function showhelp() {
@@ -27,11 +26,7 @@ function showhelp() {
 }
 
 optims=( "-O0" "-O1" "-O2" "-O3" "-Os" "-Ofast" )
-if [ $# -eq 0 ]
-then
-  make
-  runprogram $? "-O0"
-elif [ "$1" = "help" ] || [ "$1" = "-h" ] || [ "$1" = "--help" ]
+if [ $# -eq 0 ] || [ "$1" = "help" ] || [ "$1" = "-h" ] || [ "$1" = "--help" ]
 then
   showhelp
 elif [ "$1" = "clean" ]
@@ -49,57 +44,28 @@ then
       rm "out-asm${i}" "dump-asm${i}"
     fi
   done
-elif [ "$1" = "all" ]
-then
-  for i in "${optims[@]}"
-  do
-    make OLVL=$i
-    runprogram $? $i
-  done
-  sed -i "s/HammingDistance_c/HammingDistance_s/g" main.c
-  make OLVL=-O0
-  runprogram $? "-asm-O0"
-  make OLVL=-O2
-  runprogram $? "-asm-O2"
-  sed -i "s/HammingDistance_s/HammingDistance_c/g" main.c
 elif [ "$1" = "asm" ]
 then
   sed -i "s/HammingDistance_c/HammingDistance_s/g" main.c
+  mv main.c mainc.c
+  mv mains.s main.s
   make OLVL=-O0
   runprogram $? "-asm-O0"
-  sed -i "s/HammingDistance_s/HammingDistance_c/g" main.c
-elif [ "$1" = "asmO1" ]
-then
-  sed -i "s/HammingDistance_c/HammingDistance_s/g" main.c
-  make OLVL=-O1
-  runprogram $? "-asm-O1"
-  sed -i "s/HammingDistance_s/HammingDistance_c/g" main.c
-elif [ "$1" = "asmO2" ]
-then
-  sed -i "s/HammingDistance_c/HammingDistance_s/g" main.c
-  make OLVL=-O2
-  runprogram $? "-asm-O2"
-  sed -i "s/HammingDistance_s/HammingDistance_c/g" main.c
-elif [ "$1" = "main" ]
-then
-  mv main.c mainc.c
-  mv mains.s main.s
-  make OLVL=-O0
-  runprogram $? "-main-O0"
-  mv main.c mains.s
-  mv mainc.c main.c
-elif [ "$1" = "mainasm" ]
-then
-  sed -i "s/HammingDistance_c/HammingDistance_s/g" mains.s
-  mv main.c mainc.c
-  mv mains.s main.s
-  make OLVL=-O0
-  runprogram $? "-main-asm-O0"
+  make clean
   mv main.s mains.s
   mv mainc.c main.c
-  sed -i "s/HammingDistance_s/HammingDistance_c/g" mains.s
+  sed -i "s/HammingDistance_s/HammingDistance_c/g" main.c
+elif [ "$1" = "c" ]
+then
+  mv main.c mainc.c
+  mv mains.s main.s
+  make OLVL=-O0
+  runprogram $? "-O0"
+  make clean
+  mv main.s mains.s
+  mv mainc.c main.c
 else
-  make OLVL="-${1}"
-  runprogram $? "-${1}"
+  echo "else"
+  exit
 fi
 echo "finished"
